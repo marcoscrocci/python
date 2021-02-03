6ad0707b6093e535d72265de3ecea63c30876cb8
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-02-02 23:53:39 -0300

    Desafios aula 04

---
+++ desafio02.py
@@ -0,0 +1,4 @@
+dia = input('Qual o dia que você nasce? ')
+mes = input('Qual o mês que você nasce? ')
+ano = input('Qual o ano que você nasce? ')
+print('Você nasceu no dia ', dia, ' de ', mes, ' de ', ano, '. Correto?')
---
+++ desafio03.py
@@ -0,0 +1,4 @@
+print('Vamos somar dois número...')
+a = input('Digite o primeiro número: ')
+b = input('Digite o segundo número: ')
+print('A soma é ', int(a) + int(b))
