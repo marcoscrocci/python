7881bf3d87c8db5aba2e12212e8296e75ee6447c
Marcos M. Crocci <marcoscrocci@gmail.com>
2021-02-02 23:30:29 -0300

    Scripts 001

---
+++ teste001.py
@@ -0,0 +1,4 @@
+print('Novo teste')
+print(7+4)
+print('7'+'4')
+print('Olá', 5)
---
+++ teste002_variaveis.py
@@ -0,0 +1,4 @@
+nome=input('Qual é o seu nome? => Resposta: ')
+idade=input('Qual é a sua idade? => Resposta: ')
+peso=input('Qual é o seu peso? => Resposta: ')
+print(nome, idade, peso)
